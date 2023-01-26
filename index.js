672d936d12dae73a396069d7e38c184385e27f32
trteel1 <tateelamino@gmail.com>
2023-01-26 19:50:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const text = document.querySelector('#text')
+function UpdateInner() {
+    text.innerHTML = "This is really cool!"
+}
+
+function alertClick() {
+    text.innerHTML = 'Hey, You find the Default Text: "JavaScript is so cool. It lets me add text to my page programmatically."'
+}
+
+
+document.addEventListener('DOMContentLoaded', UpdateInner);
+
+text.addEventListener('click', alertClick);
